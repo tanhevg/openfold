f7357991c5b5c42063d8edff7c5480fd9e105de5
Evgeny Tankhilevich <et517@ic.ac.uk>
2023-01-27 17:03:37 +0000

    exclude computed models from cluster file

--- scripts_et/pdb_clusters_no_comp.sh
+++ scripts_et/pdb_clusters_no_comp.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # process the sequence clusters file from RCSB to remove the computed structures (AlphaFold and ModelArchive)
 # https://www.rcsb.org/docs/programmatic-access/file-download-services#sequence-clusters-data
+# https://www.rcsb.org/docs/grouping-structures/sequence-based-clustering
 SEQ_ID_PCT=40
 FILE_WITH_COMP="clusters-by-entity-${SEQ_ID_PCT}.txt"
 FILE_WITHOUT_COMP="clusters-by-entity-${SEQ_ID_PCT}-no-comp.txt"
